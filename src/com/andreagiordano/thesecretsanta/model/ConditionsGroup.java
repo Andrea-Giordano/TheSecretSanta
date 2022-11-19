a554c4fc83dda53b1c96c12f9c116fd7cf9e86f4
Andrea Giordano <andrea.giordano.inf@gmail.com>
2022-11-19 14:16:17 +0100

    improvements

--- src/com/andreagiordano/thesecretsanta/model/ConditionsGroup.java
+++ src/com/andreagiordano/thesecretsanta/model/ConditionsGroup.java
@@ -6,18 +6,16 @@ import java.util.List;
 import java.util.Random;
 
 public class ConditionsGroup {
-    List<String> originalConditions;
     List<String> conditions;
 
     public ConditionsGroup() {
-        this.originalConditions = ConditionsConfig.initConditions();
         this.conditions = ConditionsConfig.initConditions();
     }
 
     public String pickOne() {
         Random rnd = new Random();
-        if (conditions.size() == 1) {
-            return originalConditions.get(rnd.nextInt(originalConditions.size()));
+        if (conditions.size() == 0) {
+            conditions = ConditionsConfig.initConditions();
         }
         return conditions.remove(rnd.nextInt(conditions.size()));
     }
